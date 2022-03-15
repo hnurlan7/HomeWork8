10fad460e194794fb6a2d673fe368cf2d3981538
hnurlan7 <tu6f7xcr3@code.edu.az>
2022-03-16 01:39:31 +0400

    added task8

---
+++ HomeWork8/Models/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork8.Models
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                if (_age < 0)
+                {
+                    Console.WriteLine("yas 0dan kicik ola bilmez");
+                }
+                return _age;
+            }
+            set
+            {
+                //if (value > 0)
+                //{
+                //    _age = value;
+                //}
+                _age = value;
+            }
+        }
+
+        public bool result;
+        private string password(string _password)
+        {
+            foreach (string pass in _password)
+
+
+            {
+                result = Char.IsUpper(pass);
+            }
+            if (result != true)
+            {
+                return "En az 1boyuk herf daxil olunmalidir";
+
+            }
+            return _password;
+        }
+        public string Password { get; set; }
+
+
+
+
+        public User(string username, int _age, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Age = _age;
+        }
+        public void getInfo()
+        {
+            Console.WriteLine($"{Username} {_age} {Password}");
+        }
+
+
+
+    }
+}
---
+++ HomeWork8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HomeWork8.Models;
+
+namespace HomeWork8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("user", -20, "pass");
+            Console.WriteLine(user.Age);
+        }
+    }
+}
